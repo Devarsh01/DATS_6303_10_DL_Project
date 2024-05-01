cfd9152a9841040ede18a04ee48ad6f0bc648a68
DishaKacha7 <142935758+DishaKacha7@users.noreply.github.com>
2024-05-01 11:11:51 -0400

    Update Project_Streamlit_Final.py

--- Project_Streamlit.py
+++ Project_Streamlit.py
@@ -14,14 +14,48 @@ from midi2audio import FluidSynth
 import mido
 from scipy import signal
 import tempfile
+bg_image = "/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/piano.webp"
+
+#st.image(bg_image, use_column_width=True)
+st.image(bg_image, use_column_width=True, width="100%")
+# # Check for GPU availability
+# if tf.config.experimental.list_physical_devices('GPU'):
+#     st.write("Using GPU")
+# else:
+#     st.write("Using CPU")
+
+# Set the background image as the page background
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url("{bg_image}");
+        background-size: cover;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }}
+    .title {{
+        color: white;
+        font-size: 3em;
+        text-align: center;
+    }}
+    .intro {{
+        color: white;
+        font-size: 1.5em;
+        text-align: center;
+        margin-top: 20px;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
-# Check for GPU availability
-if tf.config.experimental.list_physical_devices('GPU'):
-    st.write("Using GPU")
-else:
-    st.write("Using CPU")
-
+# Display the title
+st.markdown("<h1 class='title'>Classical Music Generation</h1>", unsafe_allow_html=True)
+st.markdown("<p class='intro' style='font-size: 14px; text-align: justify;'>Welcome to our Classical Music Generation application! This project leverages Long Short-Term Memory (LSTM) neural networks with embedding layers to generate classical music sequences. Using MIDI files as input, the application extracts detailed note sequences, including pitch, velocity, start, and end times. These sequences are then used to train the LSTM model, which learns patterns in the music data. Once trained, the model can generate new music sequences based on a provided seed sequence. By adjusting parameters such as temperature, users can control the variability and creativity of the generated music. The generated music sequences are then converted to MIDI format to ensure all notes are audible. With this application, users can explore the fascinating world of classical music generation, creating unique compositions with just a click of a button!</p>", unsafe_allow_html=True)
 
 @st.cache_resource
 def load_midi_details(directory):
@@ -80,7 +114,7 @@ def generate_music(model, seed_sequence, length=10, steps_per_second=5, temperat
         # Append the predicted pitch to the generated sequence
         generated_sequence = np.vstack([generated_sequence, predicted_pitch])
 
-    st.write("Generated sequence with variability:", generated_sequence[-30:])
+    #st.write("Generated sequence with variability:", generated_sequence[-30:])
     return generated_sequence
 
 def generated_to_midi(generated_sequence, fs=100, total_duration=6):
@@ -127,46 +161,92 @@ def midi_to_wav(midi_path, wav_path):
 
 # Define the directory options and their corresponding model paths
 composer_models = {
-    'albeniz': '/home/ubuntu/sai/models/albeniz.h5',
-    'bach': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/model_bach.h5',
-    'balakirew': '/path/to/balakirew_model.h5'
+    'albeniz': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/albeniz.h5',
+    'bach': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/bach.h5',
+    'balakir': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/balakir.h5',
+    'beeth':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/beeth.h5',
+    'borodin':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/borodin.h5',
+    'brahms':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/brahms.h5',
+    'burgm':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/burgm.h5',
+    'chopin':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/chopin.h5',
+    'debussy':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/debussy.h5',
+    'granados':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/granados.h5',
+    'grieg':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/grieg.h5',
+    'haydn':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/haydn.h5',
+    'liszt':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/liszt.h5',
+    'mendelssohn':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/mendelssohn.h5',
+    'mozart':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/mozart.h5',
+    'muss':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/muss.h5',
+    'schubert':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/schubert.h5',
+    'schumann':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/schumann.h5',
+    'tschai':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/Model/tschai.h5'
+
 }
 
 # Define the directory options for training and prediction MIDI files
 training_midi_directory_options = {
-    'albeniz': '/home/ubuntu/sai/Data Sources/albeniz',
+    'albeniz': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/albeniz',
     'bach': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/bach',
-    'balakir': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/balakir'
+    'balakir': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/balakir',
+    'beeth':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/beeth',
+    'borodin':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/borodin',
+    'brahms':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/brahms',
+    'burgm':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/burgm',
+    'chopin':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/chopin',
+    'debussy':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/debussy',
+    'granados':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/granados',
+    'grieg':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/grieg',
+    'haydn':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/haydn',
+    'liszt':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/liszt',
+    'mendelssohn':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/mendelssohn',
+    'mozart':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/mozart',
+    'muss':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/muss',
+    'schubert':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/schubert',
+    'schumann':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/schumann',
+    'tschai':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/tschai'
 }
 
 prediction_midi_directory_options = {
-    'albeniz': '/home/ubuntu/sai/Data Sources/albeniz',
-    'bach': '/home/ubuntu/sai/Data Sources/bach',
-    'balakir': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/balakir'
+    'albeniz': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/albeniz',
+    'bach': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/bach',
+    'balakir': '/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/balakir',
+    'beeth':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/beeth',
+    'borodin':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/borodin',
+    'brahms':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/brahms',
+    'burgm':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/burgm',
+    'chopin':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/chopin',
+    'debussy':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/debussy',
+    'granados':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/granados',
+    'grieg':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/grieg',
+    'haydn':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/haydn',
+    'liszt':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/liszt',
+    'mendelssohn':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/mendelssohn',
+    'mozart':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/mozart',
+    'muss':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/muss',
+    'schubert':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/schubert',
+    'schumann':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/schumann',
+    'tschai':'/Users/dishakacha/Downloads/Deep_Learning/Deep_Learing/Project/archive/tschai'
 }
 
-st.title("Classical Music Generation")
+#st.markdown('<div class="center"><h1>Classical Music Generation</h1></div>', unsafe_allow_html=True)
 
 # Select composer for training
 selected_composer_training = st.selectbox("Select composer for training:", list(composer_models.keys()))
 training_directory_path = training_midi_directory_options[selected_composer_training]
 
-# Select composer for prediction
-selected_composer_prediction = st.selectbox("Select composer for prediction:", list(composer_models.keys()))
-prediction_directory_path = prediction_midi_directory_options[selected_composer_prediction]
-
 # Parameters for the sequence creation
 seq_length = 30  # Length of the input sequences
 vocab_size = 128  # Number of unique pitches (for MIDI, typically 128)
+
 
 if st.button('Load Data'):
     # Load training data
     training_sequences = load_midi_details(training_directory_path)
     training_input_sequences, training_target_sequences = create_input_target_sequences(training_sequences, seq_length,
                                                                                         vocab_size)
-    st.write(f"Loaded {len(training_sequences)} training sequences.")
-    st.write(f"Training input sequences shape: {training_input_sequences.shape}")
-    st.write(f"Training target sequences shape: {training_target_sequences.shape}")
+    #st.write(f"Loaded {len(training_sequences)} training sequences.")
+    #st.write(f"Training input sequences shape: {training_input_sequences.shape}")
+    #st.write(f"Training target sequences shape: {training_target_sequences.shape}")
 
     # Load the selected model for training
     model_path = composer_models[selected_composer_training]
@@ -176,6 +256,12 @@ if st.button('Load Data'):
     except Exception as e:
         st.error(f"Error loading training model: {e}")
 
+# Select composer for prediction
+selected_composer_prediction = st.selectbox("Select composer for prediction:", list(composer_models.keys()))
+prediction_directory_path = prediction_midi_directory_options[selected_composer_prediction]
+
+temperature = st.slider("Temperature", min_value=0.0, max_value=5.0, step=0.1)
+
 
 # Generate music based on the loaded model
 if st.button('Generate Music'):
@@ -184,9 +270,9 @@ if st.button('Generate Music'):
     prediction_sequences = load_midi_details(prediction_directory_path)
     prediction_input_sequences, prediction_target_sequences = create_input_target_sequences(prediction_sequences,
                                                                                             seq_length, vocab_size)
-    st.write(f"Loaded {len(prediction_sequences)} prediction sequences.")
-    st.write(f"Prediction input sequences shape: {prediction_input_sequences.shape}")
-    st.write(f"Prediction target sequences shape: {prediction_target_sequences.shape}")
+    #st.write(f"Loaded {len(prediction_sequences)} prediction sequences.")
+    #st.write(f"Prediction input sequences shape: {prediction_input_sequences.shape}")
+    #st.write(f"Prediction target sequences shape: {prediction_target_sequences.shape}")
 
     training_model = load_existing_model(composer_models[selected_composer_training])
 
